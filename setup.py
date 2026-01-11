002a0387c34a12e4fa832215958baf2cf076660f
AbhyanandSharma2005 <163466061+AbhyanandSharma2005@users.noreply.github.com>
2026-01-12 00:02:26 +0530

    new added

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages ,setup
+# from typing import List
+# from typing import List
+
+def get_requirements()->list[str]:
+    
+    requirements_list = list[str] =[]
+    
+    return requirements_list
+
+setup(
+name = 'Aps---Sensor',  
+version="0.0.1",
+author="Abhyanand",
+author_email="abhyanandsharma827@gmail.com",
+packages=find_packages(),
+install_requires = get_requirements() , #['pymongo']
+  
+    
+    
+    
+)
